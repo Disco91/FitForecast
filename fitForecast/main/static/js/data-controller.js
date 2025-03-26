02e813791dbae79b1ef04599c2f8b780347b8410
Disco91 <dean_davis91@live.co.uk>
2025-03-26 23:50:17 +1100

    Improved Smart Index Calculations

--- fitForecast/main/static/js/data-controller.js
+++ fitForecast/main/static/js/data-controller.js
@@ -1,3 +1,4 @@
+let weatherDataDict = {};
 let temperature = -99;
 let wind = -99;
 let uv = -99;
@@ -9,13 +10,14 @@ function fetchWeatherData() {
     fetch('/weather/')
         .then(response => response.json()) // Parse the response as JSON
         .then(data => {
-            console.log("Fetched Data:", data);
+            
             // Update the page with the weather data
             temperature = data.temperature;
             wind = data.wind;
             uv = data.uv;
             condition = data.condition;
             conditionLater = data.conditionLater;
+            weatherDataDict = data;
 
             // After retrieving weather data, update index
             const result = run_index(temperature, wind, uv, condition, conditionLater);
@@ -28,6 +30,8 @@ function fetchWeatherData() {
             // prepare data to tiles.js
             const eventTiles = new CustomEvent("WeatherDataUpdatedTiles",{ detail: { temperature, wind, uv, condition, conditionLater, result }});
             document.dispatchEvent(eventTiles)
+            
+            console.log("Fetched Data2:", weatherDataDict);
 })};
 
 // function to linearly score the value from 0 - 1
@@ -64,50 +68,95 @@ function favourable_condition_score(condition) {
     return scores[condition] ?? 0; // Return 0 if condition is not in scores
 }
 
-// calculate the run_index score
-function run_index(temperature, wind, uv, condition) {
-    const temp_range = [10,25]; // optimum temperature range
-    const danger_temp_range = [-10, 50]; // max and min temps before score is set to 0
-    const wind_range = [0, 30]; // optimum wind range
-    const danger_wind_limit = 50; // max and min temps before score is set to 0
-    const uv_range = [0, 5]; // optimum UV Range
-
-    if (temperature == -99 || wind == -99 || uv == -99) {
-        return "No Data"
-    }
-
-    let temp_score = 0;
-    // if temp range 
-    if (temperature < temp_range[0]) {
-        temp_score += (temperature - temp_range[0])
-    } else if (temperature > temp_range[1]) {
-        temp_score -= (temperature - temp_range[1])
+function calc_range_scores(value, range, idealScore) {
+    // Given an upper and lower limit set in variable 'range', calculate score. Out of range is negative score, within range equals 'idealScore'.
+    if (value < range[0]) {
+        calc_score += (value - range[0])
+    } else if (value > range[1]) {
+        calc_score -= (value - range[1])
     } else {
-        temp_score = 30; // ideal temp range
+        calc_score = idealScore;
+    }
+    return calc_score;
+}
+
+function calc_index_score(weatherData, temperatureRange, temperatureDangerRange, windRange, windDangerLimit, uvRange) {
+    
+    // declare empty scores for calculating final index
+    let temp_score = 0;
+    let wind_score = 0;
+    let uv_score = 0;
+    let condition_score = 0;
+
+    // calc temperature score
+    calc_range_scores(weatherData.temperature, temperatureRange, 30);
+
+    // calc wind score
+    calc_range_scores(weatherData.wind, temperatureRange, 30);
+
+    // calc uv score. If uv is higher than upper range return negative score. If UV is within recomended range return positive score.
+    if (uvRange[1] < weatherData.uv) {
+        uv_score = - 20;
+    } else {
+        uv_score = 20;
     }
 
-    let wind_score = 1 - normalize(wind, wind_range)*10; // Less wind is better
-    
-    let uv_score = 1 - normalize(uv, uv_range);       // Lower UV is better
-    uv_score -= (uv - 5 * 2); // every uv above 5 lowers score by 2* per UV.
+    // calc condition score
+    condition_score = favourable_condition_score(condition);
 
-    let condtion_score = favourable_condition_score(condition); // return condition score
+    let index = temp_score + wind_score + uv_score + condtion_score;
+
+    // danger zones return 0 score
+    // if temperature is greater than danger limit
+    if (weatherData.temperature < temperatureDangerRange[0]) {
+        index = 0;
+    } else if (weatherData.temperature > temperatureDangerRange[1]) {
+        index = 0;
+    }
+    // if wind is greater than danger limit
+    if (weatherData.wind > windDangerLimit) {
+        index = 0;
+    }
 
     console.log("condtion_score: " + condtion_score);
     console.log("temp_score: " + temp_score);
     console.log("wind_score: " + wind_score);
     console.log("uv_score: " + uv_score);
 
-    let index = temp_score + wind_score + uv_score + condtion_score;
-
-    console.log("index: " + index);
-    //if (index < 0) {
-    //    index = 0;
-    //} else if (index > 100) {
-    //    index = 100;
-    //}
-
+    // apply upper limit of 100 and lower limit of 1. Round to nearest whole number. return value.
     return Math.round(Math.min(100,Math.max(0,index)));
+}
+
+// calculate the run_index score
+function run_index(weatherDataDict) {
+    const temp_range = [10,25]; // optimum temperature range
+    const danger_temp_range = [-10, 50]; // min and max temps before score is set to 0
+    const wind_range = [0, 30]; // optimum wind range
+    const danger_wind_limit = 50; // max and min temps before score is set to 0
+    const uv_range = [0, 5]; // optimum UV Range
+
+    // calculate runIndex
+    let runIndex = calc_index_score(weatherDataDict, temp_range, danger_temp_range,
+         wind_range, danger_wind_limit, uv_range);
+
+    console.log("run Index: " + runIndex);
+    return runIndex;
+}
+
+// calculate the Cycling_index score
+function cycling_index(weatherDataDict) {
+    const temp_range = [10,25]; // optimum temperature range
+    const danger_temp_range = [0, 50]; // max and min temps before score is set to 0
+    const wind_range = [0, 20]; // optimum wind range
+    const danger_wind_limit = 40; // max and min temps before score is set to 0
+    const uv_range = [0, 5]; // optimum UV Range
+
+    // calculate runIndex
+    let cycleIndex = calc_index_score(weatherDataDict, temp_range, danger_temp_range,
+         wind_range, danger_wind_limit, uv_range);
+
+    console.log("run Index: " + runIndex);
+    return cycleIndex;
 }
 
 // update the fields in HTML with the results
